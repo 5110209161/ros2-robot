ad668b911e206451250ab58b84b830783a006aac
Herbert <hbin6358@163.com>
2025-01-15 10:55:25 +0800

    Update tm5-900_moveit_config, add run_move_group launch

--- Techman/tm5-900_moveit_config/launch/tm5-900_moveit.launch.py
+++ Techman/tm5-900_moveit_config/launch/tm5-900_moveit.launch.py
@@ -7,123 +7,122 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # Initialize Arguments
-    tm_robot_type = "tm5-900"
-    xacro_path = "config/tm5-900.urdf.xacro"
-    moveit_config_path = "tm5-900_moveit_config"    
-    srdf_path = 'config/tm5-900.srdf'
-    rviz_path = "/rviz/moveit.rviz"
-    moveit_controller_path = "config/moveit2_controllers.yaml"
-    ros2_control_controller_path = "config/ros2_controllers.yaml"
-    joint_limits_path = "config/joint_limits.yaml"
+  # Initialize Arguments
+  tm_robot_type = "tm5-900"
+  xacro_path = "config/tm5-900.urdf.xacro"
+  moveit_config_path = "tm5-900_moveit_config"    
+  srdf_path = 'config/tm5-900.srdf'
+  rviz_path = "/rviz/moveit.rviz"
+  moveit_controller_path = "config/moveit2_controllers.yaml"
+  ros2_control_controller_path = "config/ros2_controllers.yaml"
+  joint_limits_path = "config/joint_limits.yaml"
 
-    # *********************** MoveIt!2 *********************** #
-    # MoveIt configuration
-    moveit_config = (
-        MoveItConfigsBuilder(tm_robot_type)
-        .robot_description(file_path=xacro_path)
-        .robot_description_semantic(file_path=srdf_path)
-        .trajectory_execution(file_path=moveit_controller_path)
-        .joint_limits(file_path=joint_limits_path)
-        .to_moveit_configs()
-    )
+  # *********************** MoveIt!2 *********************** #
+  # MoveIt configuration
+  moveit_config = (
+    MoveItConfigsBuilder(tm_robot_type)
+    .robot_description(file_path=xacro_path)
+    .robot_description_semantic(file_path=srdf_path)
+    .trajectory_execution(file_path=moveit_controller_path)
+    .joint_limits(file_path=joint_limits_path)
+    .to_moveit_configs()
+  )
 
-    # Move group node/action server
-    move_group_node = Node(
-        package='moveit_ros_move_group',
-        executable='move_group',
-        output='screen',
-        parameters=[
-            moveit_config.to_dict(),
-            {"use_sim_time": True}, 
-        ]
-    )
+  # Move group node/action server
+  move_group_node = Node(
+    package='moveit_ros_move_group',
+    executable='move_group',
+    output='screen',
+    parameters=[
+      moveit_config.to_dict(),
+      {"use_sim_time": True}, 
+    ]
+  )
 
-    # RViz config
-    rviz_config_file = (
-        get_package_share_directory(moveit_config_path) + rviz_path
-    )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-        parameters=[
-            moveit_config.robot_description,
-            moveit_config.robot_description_semantic,
-            moveit_config.planning_pipelines,
-            moveit_config.robot_description_kinematics,
-            moveit_config.joint_limits,
-            {"use_sim_time": True}, 
-        ]
-    )
+  # RViz config
+  rviz_config_file = (
+    get_package_share_directory(moveit_config_path) + rviz_path
+  )
+  rviz_node = Node(
+    package="rviz2",
+    executable="rviz2",
+    name="rviz2",
+    output="log",
+    arguments=["-d", rviz_config_file],
+    parameters=[
+      moveit_config.robot_description,
+      moveit_config.robot_description_semantic,
+      moveit_config.planning_pipelines,
+      moveit_config.robot_description_kinematics,
+      moveit_config.joint_limits,
+      {"use_sim_time": True}, 
+    ]
+  )
 
-    # ***** STATIC TRANSFORM ***** #
-    # Static TF node: publish world -> base transform
-    static_tf = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        name="static_transform_publisher",
-        output="log",
-        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'world', 'base']
-    )
+  # ***** STATIC TRANSFORM ***** #
+  # Static TF node: publish world -> base transform
+  static_tf = Node(
+    package="tf2_ros",
+    executable="static_transform_publisher",
+    name="static_transform_publisher",
+    output="log",
+    arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'world', 'base']
+  )
 
-    # Robot State Publisher Node: Publishes tf's for the robot
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='both',
-        parameters=[
-            moveit_config.robot_description,
-            {"use_sim_time": True}, 
-        ],
-    )
+  # Robot State Publisher Node: Publishes tf's for the robot
+  robot_state_publisher = Node(
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    name='robot_state_publisher',
+    output='both',
+    parameters=[
+      moveit_config.robot_description,
+      {"use_sim_time": True}, 
+    ],
+  )
 
-    # ***** ROS2_CONTROL -> LOAD CONTROLLERS ***** #
-    ros2_controllers_path = os.path.join(
-        get_package_share_directory(moveit_config_path),
-        ros2_control_controller_path
-    )
+  # ***** ROS2_CONTROL -> LOAD CONTROLLERS ***** #
+  ros2_controllers_path = os.path.join(
+    get_package_share_directory(moveit_config_path),
+    ros2_control_controller_path
+  )
 
-    ros2_control_node = Node(
-        package='controller_manager',
-        executable='ros2_control_node',
-        parameters=[ros2_controllers_path],
-        remappings=[
-            ('/controller_manager/robot_description', '/robot_description'),
-        ],
-        output='both',
-    )
+  ros2_control_node = Node(
+    package='controller_manager',
+    executable='ros2_control_node',
+    parameters=[ros2_controllers_path],
+    remappings=[
+      ('/controller_manager/robot_description', '/robot_description'),
+    ],
+    output='both',
+  )
 
-    # Joint STATE BROADCASTER:
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
-    )
+  # Joint STATE BROADCASTER:
+  joint_state_broadcaster_spawner = Node(
+    package="controller_manager",
+    executable="spawner",
+    arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+  )
 
-    # Joint TRAJECTORY Controller:
-    tm_arm_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "tmr_arm_controller",
-            "--controller-manager",
-            "/controller_manager"
-        ]
-    )
+  # Joint TRAJECTORY Controller:
+  tm_arm_controller_spawner = Node(
+    package="controller_manager",
+    executable="spawner",
+    arguments=[
+      "tmr_arm_controller",
+      "--controller-manager",
+      "/controller_manager"
+    ]
+  )
 
-    return LaunchDescription(
-        [
-            rviz_node,
-            static_tf,
-            robot_state_publisher,
-            move_group_node,
-            ros2_control_node,
-            joint_state_broadcaster_spawner,
-            tm_arm_controller_spawner,
-        ]
-    )
-
+  return LaunchDescription(
+    [
+      rviz_node,
+      static_tf,
+      robot_state_publisher,
+      move_group_node,
+      ros2_control_node,
+      joint_state_broadcaster_spawner,
+      tm_arm_controller_spawner,
+    ]
+  )
